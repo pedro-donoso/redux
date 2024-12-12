ef01d7e0f709831d8734bd134a5a92a06fd57a11
pedro-donoso <pedro.donoso.r@usach.cl>
2024-12-12 12:25:16 -0300

    creo userSlice

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
+import { Provider } from 'react-redux';
+
 import App from './App.jsx'
+import store from './redux/store.js';
+import './index.css'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
 )
