ddb27415deb85e642c86845319fdd617b23e8fd4
pedro-donoso <pedro.donoso.r@usach.cl>
2024-12-12 13:07:04 -0300

    agrego url

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import { Header } from "./components/Header";
 import { Email } from "./components/Email";
-import './App.css'
+import { addUser } from "./redux/userSlice";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import './App.css';
 
 function App() {
+  const dispatch = useDispatch();
 
-  return <div className='App'></div>;
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users/1")
+      .then((response) => response.json())
+      .then((data) => dispatch(addUser(data)))
+
+  return (
+    <div className='App'>
+      <Header />
+      <Email />
+    </div>
+  ); 
 }
 
 export default App;
